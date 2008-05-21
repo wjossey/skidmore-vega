771618d9932f9a84455e4964db6eaf6cfa894fcf
weston.jossey <weston.jossey@d83795a1-044c-0410-9bf6-ad80259c2566>
2008-05-21 15:55:15 +0000

    Renaming files

---
+++ trunk/VEGA/src/vega/graph/edge/UndirectedEdgeImpl.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package vega.graph.edge;
+
+import vega.graph.vertex.Vertex;
+
+/**
+ * The UndirectedEdge class is actually just a repeat of the Edge class.  By default
+ * an edge is said to be undirected, so this class is actually just a placeholder so
+ * that developers can easily distinguish if an edge is directed or undirected.
+ * @author w_jossey
+ */
+public class UndirectedEdge extends EdgeImpl{
+    
+    /**
+     * See Edge class for documentation.
+     * @param a
+     * @param b
+     */
+    public UndirectedEdge(Vertex a, Vertex b){
+        super(a, b);
+    }
+    
+    /**
+     * See Edge class for documentation.
+     * @param a
+     * @param b
+     * @param weight
+     */
+    public UndirectedEdge(Vertex a, Vertex b, double weight){
+        super(a, b, weight);
+    }
+    
+}
